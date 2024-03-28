00551674dbff03aec85487586d58f582f94305a4
Chigozie-css <chigoziedivine62@gmail.com>
2024-03-28 13:30:18 -0700

    Sorting issues

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -15,7 +15,7 @@
  *         - If there is no ancestor node
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+		const binary_tree_t *second)
 {
 	size_t depth_first, depth_second;
 
